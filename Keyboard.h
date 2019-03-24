27e308d6b807db46f14cc7516f9216d66b2c7830
Masayuki Sakura <webmaster@tool-lab.com>
2019-03-24 10:55:02 +0900

    first commit

---
+++ Keyboard.h
@@ -0,0 +1,16 @@
+/* 
+ * File:   Keyboard.h
+ * Copyright 2013 Tool Labs
+ */
+
+#ifndef KEYBOARD_H
+#define	KEYBOARD_H
+
+// Definition for switch pin
+#define switch1     PORTAbits.RA3 // Also used for MCLR pin
+#define switch2     PORTCbits.RC7
+#define switch3     PORTCbits.RC6
+#define switch4     PORTCbits.RC5
+#define switch5     PORTCbits.RC4
+
+#endif	/* KEYBOARD_H */
